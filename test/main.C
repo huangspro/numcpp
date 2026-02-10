b210af545d126f813f24bfd808d654668c32108a
hhy <13612276628@163.com>
2026-02-10 10:00:50 +0800

    3.0

--- test/main.C
+++ test/main.C
@@ -3,7 +3,7 @@
 int main(){
   numcpp a1({3, 4, 2}, 5);
   numcpp a2({3, 4, 2}, 2);
-  //numcpp a3 = a1 + a2;
-  //std::cout<<a3.get({1, 1, 1});
+  numcpp a3 = a1 + a2;
+  std::cout<<a3.get({1, 1, 1});
   return 0;
 }
