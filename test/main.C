a34bbf23e6662ba8165816cf22c05cb058d02ba6
hhy <13612276628@163.com>
2026-02-10 11:22:34 +0800

    0

--- test/main.C
+++ test/main.C
@@ -1,8 +1,7 @@
 #include "../include/numcpp.h"
 
 int main(){
-  numcpp a1({300, 400, 2}, 0, 100, "int");
-  numcpp a2({300, 400, 2}, 0, 300, "int");
-  std::cout<<(a1 + 10000).get({0, 2, 2});
+  numcpp a1({3, 4, 2}, 0, 100, "int");
+  a1.print();
   return 0;
 }
