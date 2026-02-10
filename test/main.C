163ebdb65d219a581492ebbbbc359032e8635db2
hhy <13612276628@163.com>
2026-02-10 10:40:15 +0800

    1.0

--- test/main.C
+++ test/main.C
@@ -3,6 +3,6 @@
 int main(){
   numcpp a1({300, 400, 2}, 0, 100, "int");
   numcpp a2({300, 400, 2}, 0, 300, "int");
-  std::cout<<a1.get({0, 2, 2});
+  std::cout<<(a1 + 10000).get({0, 2, 2});
   return 0;
 }
