8e23cf744b23c8e78ba7885fa83c9e96e7f9e71e
huangspro <165527467+huangspro@users.noreply.github.com>
2026-02-13 07:59:51 +0800

    Update main.C

--- test/main.C
+++ test/main.C
@@ -1,9 +1,13 @@
 #include "../include/numcpp.h"
 
 int main(){
-  numcpp a1=numcpp::normal({4, 3, 2, 5}, 0, 1);
+  numcpp a1=numcpp::normal({4, 3, 2}, 0, 1);
+  std::cout<<a1. number<<std::endl<<std::flush;
   a1.print();
-  numcpp b1 = a1.get({1});
-  b1.print();
+  std::cout<<a1. number<<std::endl<<std::flush;
+  a1.reshape({2, 3, 4});
+std::cout<<a1. number<<std::endl<<std::flush;
+  a1.print();
+std::cout<<a1. number<<std::endl<<std::flush;
   return 0;
 }
