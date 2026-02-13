5e4d662d21848b1228044e5fc5d17d2a506075e1
huangspro <165527467+huangspro@users.noreply.github.com>
2026-02-13 10:07:59 +0800

    Update main.C

--- test/main.C
+++ test/main.C
@@ -2,9 +2,6 @@
 
 int main(){
   numcpp a1=numcpp::normal({4, 3, 2}, 0, 1);
-  numcpp a2=numcpp::normal({2, 12}, 0, 1);
-  a1.reshape({2, 12});
   a1.print();
-  std::cout<<"..."<<a1.shape[12];
-  return 0;
+  a1[{0, 1}].print();
 }
