4ca7d4534e0fbc0953dec227db8336e96961f638
hhy <13612276628@163.com>
2026-02-15 11:27:17 +0800

    0

--- test/main.C
+++ test/main.C
@@ -1,7 +1,8 @@
 #include "../include/numcpp.h"
 
 int main(){
-  numcpp a1=numcpp::normal({4, 3, 2}, 0, 1);
+  numcpp a1=numcpp::normal({3, 2, 2}, 0, 1);
   a1.print();
+  std::cout<<a1[{0}].dimension<<std::endl<<std::flush;
   a1.get({0}).print();
 }
